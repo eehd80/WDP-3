0433cbd4a15eb2c420690ea0cc98d61e45757d25
eehd80 <eehd80yul@gmail.com>
2021-04-01 13:26:46 +0900

    swiper

--- !!setting/html-name/dist/js/comm.js
+++ !!setting/html-name/dist/js/comm.js
@@ -58,5 +58,21 @@ $(function(){
         })
     })
 
+    // slide-top
+    var swiper = new Swiper('.swiper-container', {
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        },
+      });
+
 
 })
