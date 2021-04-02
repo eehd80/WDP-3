c7348741f4557aa9c2049cdee459655d6b21548e
eehd80 <eehd80yul@gmail.com>
2021-04-02 13:30:27 +0900

    Add files via upload

--- !!setting/html-name/dist/js/comm.js
+++ !!setting/html-name/dist/js/comm.js
@@ -68,10 +68,10 @@ $(function(){
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev',
         },
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-        },
+        // autoplay: {
+        //     delay: 2500,
+        //     disableOnInteraction: false,
+        // },
         pagination: {
         el: '.swiper-pagination',
         clickable: true,
